e3a41147115f77fa790fa1e3c555f7cdf46cee65
Shilpa Ram <shilparam696@gmail.com>
2023-01-17 12:02:13 +0530

    LinkedList Examples

---
+++ src/main/java/array/linkedList/ListNode.java
@@ -0,0 +1,25 @@
+package array.linkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+
+            return head;
+        }
+}
+
