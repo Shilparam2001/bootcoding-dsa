6b7b2c715477e07c35b6349ab59ba1cd0502680d
Shilpa Ram <shilparam696@gmail.com>
2023-02-03 18:11:00 +0530

    Tree Examples

---
+++ src/main/java/Tree/BinaryTree.java
@@ -0,0 +1,18 @@
+package Tree;
+
+import array.linkedList.Node;
+
+public class BinaryTree {
+
+        int data;
+    BinaryTree left;
+    BinaryTree right;
+
+        BinaryTree(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+
+    }
---
+++ src/main/java/Tree/Order.java
@@ -0,0 +1,55 @@
+package Tree;
+
+public class Order {
+    public static BinaryTree create() {
+        BinaryTree root = new BinaryTree(1);
+        BinaryTree n1 = new BinaryTree(2);
+        BinaryTree n2 = new BinaryTree(3);
+        BinaryTree n3 = new BinaryTree(4);
+        BinaryTree n4 = new BinaryTree(5);
+
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n1.right = n4;
+        return root;
+    }
+
+    public static void inOrder(BinaryTree root) {
+        if (root == null)
+            return;
+
+        inOrder(root.left);
+        System.out.println(root.data);
+        inOrder(root.right);
+    }
+
+    public static void preOrder(BinaryTree root) {
+        if (root == null)
+            return;
+        System.out.println(root.data);
+        preOrder(root.left);
+
+        preOrder(root.right);
+    }
+
+    public static void postOrder(BinaryTree root) {
+        if (root == null)
+            return;
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println(root.data);
+
+    }
+
+    public static void main(String[] args) {
+        BinaryTree root = create();
+        System.out.println("Inorder");
+        inOrder(root);
+        System.out.println("preOrder");
+        preOrder(root);
+        System.out.println("postOrder");
+        postOrder(root);
+    }
+}
