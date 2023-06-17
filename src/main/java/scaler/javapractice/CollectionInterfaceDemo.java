95a9232af351afc9e0d2986df547cc4ba32e9510
Shilpa Ram <shilparam696@gmail.com>
2023-06-17 23:47:50 +0530

    Collection Interface Example

---
+++ src/main/java/scaler/javapractice/CollectionInterfaceDemo.java
@@ -0,0 +1,23 @@
+package scaler.javapractice;
+
+import java.util.*;
+
+public class CollectionInterfaceDemo {
+
+    public static void main(String[] args) {
+        Collection<String> s1 = new ArrayList<>();
+        s1.add("Hello");   // add-(predefined method in collections Interface)
+        s1.add("World");
+
+        Collection<String> s2 = new HashSet<>();
+        s2.add("A");
+        s2.add("B");
+        s2.add("C");
+        s2.add("D");
+
+
+       // s2.addAll(s1); /method to add al the elements in ArrayList
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
